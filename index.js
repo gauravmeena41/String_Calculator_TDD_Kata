1579341b320dcf34e0475ff06ab85221bf90c0d4
gurumeena41 <gurumeena41.gm@gmail.com>
2022-03-26 10:28:08 +0530

    if there are multiple negatives, they will log with error message

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ const calculate = (nums) => {
   if (newNums.length === 1 && nums[nums.length - 1] === "\n")
     throw new Error("Input is NOT ok");
 
-  if (nums.includes("-")) throw new Error("negatives not allowed");
+  let negetiveCount = 0;
+  let negetiveNums = "";
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === "-" && /^[0-9]/.test(nums[i + 1])) {
+      negetiveCount++;
+      negetiveNums += `-${nums[i + 1]} `;
+    }
+  }
+
+  if (negetiveCount === 1) throw new Error("negatives not allowed");
+  else if (negetiveCount > 1)
+    throw new Error("negatives not allowed " + negetiveNums);
 
   nums = newNums.reduce((acc, curr) => {
     return acc + parseInt(curr);
@@ -23,3 +35,5 @@ console.log(calculate(""));
 console.log(calculate("1\n2,3"));
 console.log(calculate("//;\n1;2"));
 console.log(calculate("1,\n,-3"));
+console.log(calculate("1,\n,-3,-4"));
+console.log(calculate("1,\n"));
