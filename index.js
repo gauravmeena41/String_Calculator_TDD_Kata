2b3c467ea067e5e6c524403aa6137798ddc0a720
gurumeena41 <gurumeena41.gm@gmail.com>
2022-03-26 10:21:03 +0530

    added negetives not allowed check

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const calculate = (nums) => {
   if (newNums.length === 1 && nums[nums.length - 1] === "\n")
     throw new Error("Input is NOT ok");
 
+  if (nums.includes("-")) throw new Error("negatives not allowed");
+
   nums = newNums.reduce((acc, curr) => {
     return acc + parseInt(curr);
   }, 0);
@@ -19,3 +21,5 @@ const calculate = (nums) => {
 console.log(calculate("1,2"));
 console.log(calculate(""));
 console.log(calculate("1\n2,3"));
+console.log(calculate("//;\n1;2"));
+console.log(calculate("1,\n,-3"));
